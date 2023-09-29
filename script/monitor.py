e2c361684a777ecca671c82f62bde17869b1634c
alexandervladsemenov <61551585+alexandervladsemenov@users.noreply.github.com>
2023-09-29 12:51:39 -0400

    add IO plot

--- script/monitor.py
+++ script/monitor.py
@@ -2,15 +2,19 @@ import os
 from matplotlib import pyplot as plt
 import re
 import numpy as np
+
 path_format = "Time: 2240, Cpu usage: 0, memory usage: 119.546875 MB, disk util (0, 26071040, 0, 323584)"
 
-def isfloat(element : str):
+
+def isfloat(element: str):
     try:
         float(element)
         return True
     except ValueError:
         return False
-def get_val(line : str) -> list[float]:
+
+
+def get_val(line: str) -> list[float]:
     new_st = line.replace('(', ' ')
     new_st = new_st.replace(')', ' ')
     _data_ = re.split(', | ', new_st)
@@ -20,31 +24,55 @@ def get_val(line : str) -> list[float]:
 
 
 if __name__ == "__main__":
-    files = [os.path.join("../",x) for x in os.listdir("../") if ".txt" in x and "log" in x]
+    files = [os.path.join("../", x) for x in os.listdir("../") if ".txt" in x and "log" in x]
     print(files)
     for file in files:
         time = []
         mem = []
         cpu_usage = []
+        write_bytes = []
+        read_bytes = []
         res1 = file.find("_name_")
         res2 = file.find(".txt")
         res3 = file.find("log_pid_")
         p_name = file[res1 + len("_name_"):res2]
         pid = file[res3 + len("log_pid_"):res1]
-        with open(file,mode="r") as f:
+        with open(file, mode="r") as f:
             lines = f.readlines()
             for line in lines:
                 data = get_val(line)
                 time.append(data[0])
                 cpu_usage.append(data[1])
                 mem.append(data[2])
+                write_bytes.append(data[3])
+                read_bytes.append(data[5])
         time = np.array(time)
         mem = np.array(mem)
         cpu_usage = np.array(cpu_usage)
-        plt.plot(time,mem)
+        write_bytes = np.array(write_bytes)
+        read_bytes = np.array(read_bytes)
+        plt.plot(time, mem)
+        plt.title("Memory usage")
+        plt.xlabel("Time, ms")
+        plt.ylabel("Memory RAM, MG")
         plt.show()
         plt.close()
-        plt.plot(time,cpu_usage)
+        plt.plot(time, cpu_usage)
+        plt.title("CPU usage")
+        plt.xlabel("Time, ms")
+        plt.ylabel("CPU usage, %")
+        plt.show()
+        plt.close()
+        plt.plot(time, write_bytes)
+        plt.title("Written bytes")
+        plt.xlabel("Time, ms")
+        plt.ylabel("Bytes written, Bytes")
+        plt.show()
+        plt.close()
+        plt.plot(time, read_bytes)
+        plt.title("Read bytes")
+        plt.xlabel("Time, ms")
+        plt.ylabel("Bytes read, Bytes")
         plt.show()
         plt.close()
     pass
